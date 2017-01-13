91ce6f4c83d423edff6298beec7053a8aa2de3ea
Walker Hamilton <hey@walkerhamilton.com>
2017-01-13 14:56:09 -0600

    Changed to oEmbed method, maintained backward compatibility for sites that might still have a bunch of [ustream] shortcodes hanging out in their content.

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-Ustream for WordPress is a plugin that enables the user to add tags (short codes) on the format [ustream cid="xxxxx"] or [ustream vid="xxxxx"] in blog posts/pages and have them replaced by their video content from UStream.
+Ustream for WordPress is a plugin that enables the user to embed Ustream live and recorded embeds. Put the url for the ustream video you'd like between the embed tags.
+
+    [embed]http://www.ustream.tv/recorded/98889950[/embed]
+    [embed]http://www.ustream.tv/channel/iss-hdev-payload[/embed]
 
 This really comes in handy when your blog is on a multi site WordPress instance where you are not allowed to add iframe code etc.
 
-This plugin uses short codes and automatically replaces the [ustream cid="xxxxx"] or [ustream vid="xxxxx"] tags with the embeded video related to the given cid or vid.
+### Deprecated shortcode embed method
 
-The [ustream cid="xxxxx"] tag is used when you want to stream a live broad cast and the [ustream vid="xxxxx"] is used when to publish a recorded video.
+The deprecated method for embedding Ustream still works, but ignores all non-oEmbed info. One can add tags (short codes) on the format `[ustream cid="xxxxx"]` or `[ustream vid="xxxxx"]` in blog posts/pages and have them replaced by their video content from UStream.
+
+This deprecated method uses short codes and automatically replaces the `[ustream cid="xxxxx"]` or `[ustream vid="xxxxx"]` tags with the embedded video related to the given cid or vid.
+
+The `[ustream cid="xxxxx"]` tag is used when you want to stream a live broad cast and the `[ustream vid="xxxxx"]` is used when to publish a recorded video.
--- utream-for-wordpress.php
+++ utream-for-wordpress.php
@@ -3,29 +3,29 @@
  Plugin Name: UStream for WordPress
  Plugin URI: http://www.geckosolutions.se/blog/wordpress-plugins/
  Description: Add easy to use functionality to embed UStrem videos and broadcasts.
- Version: 1.1.1
+ Version: 2.1
  Author: Niklas Olsson
  Author URI: http://www.geckosolutions.se
  License: GPL 2.0, @see http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 class ustream_for_wordpress {
-    
-	function oembed_provider() {
-	    wp_oembed_add_provider( '#http://(www\.)?ustream.tv/*#i', 'http://www.ustream.tv/oembed', true );
-	}
 
-	// Maintain backward compatibility of this plugin
-	function get_ustream_code($atts, $content=null) {
-	    extract(shortcode_atts(array('cid' => '', 'vid' => ''), $atts));
+    function oembed_provider() {
+        wp_oembed_add_provider( '#http://(www\.)?ustream.tv/*#i', 'http://www.ustream.tv/oembed', true );
+    }
 
-	    if (is_numeric($cid)) {
-		return wp_oembed_get('http://www.ustream.tv/'.$cid);
-	    } else if (is_numeric($vid)) {
-		return wp_oembed_get('http://www.ustream.tv/'.$vid);
-	    } else {
-	      return 'Error: Neither the "cid" nor the "vid" was passed to the ustream tag!';
-	    }
+    // Maintain backward compatibility of this plugin
+    function get_ustream_code($atts, $content=null) {
+        extract(shortcode_atts(array('cid' => '', 'vid' => ''), $atts));
+
+        if (is_numeric($cid)) {
+            return wp_oembed_get('http://www.ustream.tv/channel/'.$cid);
+        } else if (is_numeric($vid)) {
+            return wp_oembed_get('http://www.ustream.tv/recorded/'.$vid);
+        } else {
+          return 'Error: Neither the "cid" nor the "vid" was passed to the ustream tag!';
+        }
     }
 }
 
